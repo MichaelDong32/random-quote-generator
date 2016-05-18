5da72fbe8719166386ad9c5a3dc1c760397bc412
MichaelDong32 <mjdong32@gmail.com>
2016-05-18 21:45:52 +1200

    random quote html, js and css files

---
+++ script.js
@@ -0,0 +1,25 @@
+var thoughts = [
+{"quote": "fhjgfhkhjgkjhkj",
+"from": "ghfghjfghjf"},
+{"quote": "fhjgfhkhjgkjhkj",
+"from": "ghfghjfghjf"},
+{"quote": "fhjgfhkhjgkjhkj",
+"from": "ghfghjfghjf"},
+{"quote": "fhjgfhkhjgkjhkj",
+"from": "ghfghjfghjf"}
+
+];
+
+
+var num = Math.random();
+console.log(num);
+totalQuotes = thoughts.length;
+console.log(totalQuotes);
+quoteNum = Math.floor(num * totalQuotes);
+console.log(quoteNum);
+
+console.log(thoughts[quoteNum].quote)
+
+
+$(".quote").append('thoughts[quoteNum].quote');
+//document.getElementById('quote').innerHTML = thoughts;
